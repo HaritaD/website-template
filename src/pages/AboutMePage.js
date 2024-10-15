804f253d53a09493b57c58c24480e5205e22d428
Harita Duggirala <haritaduggirala@dhcp-10-105-147-41.wireless.northwestern.private>
2024-10-15 14:39:22 -0500

    Added UserName

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -1,14 +1,18 @@
 import React from "react";
 import stockPhoto from "../stockphoto.jpg";
 
+
+
 const AboutMe = () => {
   return (
     <div className="flex flex-col items-center py-10 min-h-screen">
+      
       <h1 className="text-6xl font-medium mb-5">About Me</h1>
       {/* image */}
 
       {/*"about me" section */}
-      <div className="bg-blue-200 min-w-56 max-w-96 font-bold flex justify-center px-5"></div>
+      <div className=" min-w-56 max-w-96 font-bold flex justify-center px-5">
+      </div>
       {/* bio */}
       <p className="w-96 px-5"></p>
       {/* hobbies and interests */}
